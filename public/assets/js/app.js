5611fabdaca5bf8bb3cccf5c72b934f6e2f3b95f
Matthew Mulford <mhmulford0@gmail.com>
2020-05-24 18:45:37 -0500

    fixed form

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,11 +1,1 @@
-/*
-var prevent = (event) => {
-  event.preventDefault();
-};
 
-// your form
-var form = document.getElementById('extractForm');
-
-// attach event listener
-form.addEventListener('submit', prevent, true);
-*/
