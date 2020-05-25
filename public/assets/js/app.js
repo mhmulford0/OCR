6a1fe64874e9ef9f41c72df0752bdb1aa0ddeaad
Matthew Mulford <mhmulford0@gmail.com>
2020-05-25 13:10:31 -0500

    front end code update

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,1 +1,9 @@
+const form = document.getElementById('extractForm');
+const regex = '';
+let url = document.getElementById('subject').value;
 
+
+form.addEventListener('submit', (event) => {
+  //event.preventDefault();
+  console.log(url);
+});
