a9a3060bbf96c9e4e2992e6883587092cb728e41
mhmulford0 <mhmulford0@gmail.com>
2020-05-27 15:01:44 -0500

    file uploads

--- routes/upload.js
+++ routes/upload.js
@@ -1,9 +1,9 @@
-const express = require('express');
+var express = require('express');
 var router = express.Router();
 const fileUpload = require('express-fileupload');
 const app = express();
-
-// default options
+const fetch = require('node-fetch');
+var FormData = require('form-data');
 
 router.post('/', async function (req, res) {
   if (!req.files || Object.keys(req.files).length === 0) {
@@ -19,37 +19,41 @@ router.post('/', async function (req, res) {
   ) {
     if (err) return res.status(500).send(err);
 
-    res.send('File uploaded!');
+    console.log('File uploaded!');
   });
-  // let formdata = new FormData();
-  // formdata.append('language', 'eng');
-  // formdata.append('isOverlayRequired', 'false');
-  // formdata.append('base64Image', req.body.url);
-  // formdata.append('iscreatesearchablepdf', 'false');
-  // formdata.append('issearchablepdfhidetextlayer', 'false');
 
-  // let requestOptions = {
-  //   headers: {
-  //     apikey: process.env.API_KEY,
-  //   },
-  //   method: 'POST',
-  //   body: formdata,
-  //   redirect: 'follow',
-  // };
+  const buf = Buffer.from(parseFile.data, 'utf8');
+  const img64 = buf.toString('base64');
 
-  // const parseResponse = await fetch(
-  //   'https://api.ocr.space/parse/image',
-  //   requestOptions
-  // )
-  //   .then((response) => response.json())
-  //   .catch((error) => console.log('error', error));
+  let formdata = new FormData();
+  formdata.append('language', 'eng');
+  formdata.append('isOverlayRequired', 'false');
+  formdata.append('base64Image', `data:image/png;base64,${img64}`);
+  formdata.append('iscreatesearchablepdf', 'false');
+  formdata.append('issearchablepdfhidetextlayer', 'false');
 
-  // if (parseResponse.IsErroredOnProcessing == false) {
-  //   clientRes.splice(0, 1, parseResponse.ParsedResults[0].ParsedText);
-  //   res.redirect('/#extract');
-  // } else {
-  //   clientRes.splice(0, 1, `Error: ${parseResponse.ErrorMessage}`);
-  //   res.redirect('/#extract');
-  //}
+  let requestOptions = {
+    headers: {
+      apikey: process.env.API_KEY,
+    },
+    method: 'POST',
+    body: formdata,
+    redirect: 'follow',
+  };
+
+  const parseResponse = await fetch(
+    'https://api.ocr.space/parse/image',
+    requestOptions
+  )
+    .then((response) => response.json())
+    .catch((error) => console.log('error', error));
+
+  if (parseResponse.IsErroredOnProcessing == false) {
+    clientRes.splice(0, 1, parseResponse.ParsedResults[0].ParsedText);
+    res.redirect('/#extract');
+  } else {
+    clientRes.splice(0, 1, `Error: ${parseResponse.ErrorMessage}`);
+    res.redirect('/#extract');
+  }
 });
 module.exports = router;
