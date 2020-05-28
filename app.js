ee291dbd42b93b7ca75a5e8aeb3d95ab9b52788f
mhmulford0 <mhmulford0@gmail.com>
2020-05-27 19:09:19 -0500

    upload debug

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(
   fileUpload({
-    limits: { fileSize: 1000000 },
+    limits: { fileSize: 2000000 },
     abortOnLimit: true,
     responseOnLimit: 'File Size Limit Exceeded',
   })
@@ -32,14 +32,11 @@ app.use(
 app.use('/', indexRouter);
 app.use('/upload', uploadRouter);
 
-// catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
--- routes/upload.js
+++ routes/upload.js
@@ -5,6 +5,10 @@ const app = express();
 const fileUpload = require('express-fileupload');
 const fetch = require('node-fetch');
 var FormData = require('form-data');
+const imagemin = require('imagemin');
+const imageminJpegtran = require('imagemin-jpegtran');
+const imageminPngquant = require('imagemin-pngquant');
+app.use(fileUpload());
 
 router.post('/', async function (req, res) {
   if (!req.files || Object.keys(req.files).length === 0) {
@@ -13,46 +17,67 @@ router.post('/', async function (req, res) {
 
   let parseFile = req.files.parseFile;
 
-  parseFile.mv(`/mnt/c/code/OCR/public/parseimage/${parseFile.name}`, function (
-    err
-  ) {
-    if (err) return res.status(500).send(err);
+  parseFile.mv(
+    `/home/mhmulford/code/OCR/public/parseimage/${parseFile.name}`,
+    function (err) {
+      if (err) return res.status(500).send(err);
+      res.send('File uploaded!');
+    }
+  );
 
-    console.log('File uploaded!');
-  });
+  // const buf = Buffer.from(parseFile.data, 'utf8');
+  // const img64 = buf.toString('base64');
+  // console.log(img64);
 
-  const buf = Buffer.from(parseFile.data, 'utf8');
-  const img64 = buf.toString('base64');
+  // const compressedImg = await imagemin(
+  //   [`public/parseimage/${parseFile.name}.{jpg,png}`],
+  //   {
+  //     destination: 'public/images',
+  //     plugins: [
+  //       imageminJpegtran(),
+  //       imageminPngquant({
+  //         quality: [0.3, 0.3],
+  //       }),
+  //     ],
+  //   }
+  // );
+  // const compressedBuf = Buffer.from(compressedImg[0].data, 'utf8');
+  // const compressedImg64 = compressedBuf.toString('base64');
+  // console.log(compressedImg64);
+  // console.log();
 
-  let formdata = new FormData();
-  formdata.append('language', 'eng');
-  formdata.append('isOverlayRequired', 'false');
-  formdata.append('base64Image', `data:${parseFile.mimetype};base64,${img64}`);
-  formdata.append('iscreatesearchablepdf', 'false');
-  formdata.append('issearchablepdfhidetextlayer', 'false');
+  // let formdata = new FormData();
+  // formdata.append('language', 'eng');
+  // formdata.append('isOverlayRequired', 'false');
+  // formdata.append(
+  //   'base64Image',
+  //   `data:${parseFile.mimetype};base64,${compressedImg64}`
+  // );
+  // formdata.append('iscreatesearchablepdf', 'false');
+  // formdata.append('issearchablepdfhidetextlayer', 'false');
 
-  let requestOptions = {
-    headers: {
-      apikey: process.env.API_KEY,
-    },
-    method: 'POST',
-    body: formdata,
-    redirect: 'follow',
-  };
+  // let requestOptions = {
+  //   headers: {
+  //     apikey: process.env.API_KEY,
+  //   },
+  //   method: 'POST',
+  //   body: formdata,
+  //   redirect: 'follow',
+  // };
 
-  const parseResponse = await fetch(
-    'https://api.ocr.space/parse/image',
-    requestOptions
-  )
-    .then((response) => response.json())
-    .catch((error) => console.log('error', error));
+  // const parseResponse = await fetch(
+  //   'https://api.ocr.space/parse/image',
+  //   requestOptions
+  // )
+  //   .then((response) => response.json())
+  //   .catch((error) => console.log('error', error));
 
-  if (parseResponse.IsErroredOnProcessing == false) {
-    clientRes.splice(0, 1, parseResponse.ParsedResults[0].ParsedText);
-    res.redirect('/#extract');
-  } else {
-    clientRes.splice(0, 1, `Error: ${parseResponse.ErrorMessage}`);
-    res.redirect('/#extract');
-  }
+  // if (parseResponse.IsErroredOnProcessing == false) {
+  //   clientRes.splice(0, 1, parseResponse.ParsedResults[0].ParsedText);
+  //   res.redirect('/#extract');
+  // } else {
+  //   clientRes.splice(0, 1, `Error: ${parseResponse.ErrorMessage}`);
+  //   res.redirect('/#extract');
+  // }
 });
 module.exports = router;
