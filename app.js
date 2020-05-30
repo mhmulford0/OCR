158b32796be7aeca4897a93bc5919b69e3e3c645
mhmulford0 <mhmulford0@gmail.com>
2020-05-30 11:10:10 -0500

    added production 400 page

--- app.js
+++ app.js
@@ -42,7 +42,11 @@ app.use(function (err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  if (req.app.get('env') === 'development') {
+    res.render('error');
+  } else {
+    res.render('prodError');
+  }
 });
 
 module.exports = app;
